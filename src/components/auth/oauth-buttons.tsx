e34c7e1fd1fbf59ceb51aee97f36c8136c805954
nakshatra raghav <combatrickshaw@gmail.com>
2023-10-22 13:47:02 +0530

    (oauth): signin

--- src/components/auth/oauth-buttons.tsx
+++ src/components/auth/oauth-buttons.tsx
@@ -1,13 +1,41 @@
 "use client";
 
 import { useState } from "react";
+import { signIn } from "next-auth/react";
+
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
 
 export function OAuthButtons() {
   const [loading, setLoading] = useState({
     discord: false,
     google: false,
   });
+
+  const { toast } = useToast();
+
+  async function handleOAuth(provider: "discord" | "google") {
+    if (provider === "google") {
+      setLoading({
+        google: true,
+        discord: false,
+      });
+    } else {
+      setLoading({
+        google: false,
+        discord: true,
+      });
+    }
+    const response = await signIn(provider);
+
+    if (!response?.ok) {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+      });
+    }
+  }
 
   return (
     <div className="flex justify-between items-center space-x-2">
