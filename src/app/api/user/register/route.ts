4bdd0463b992611844a4638fedf31b15255b77a3
nakshatra raghav <combatrickshaw@gmail.com>
2023-10-22 13:30:40 +0530

    (register route): register the user route

---
+++ src/app/api/user/register/route.ts
@@ -0,0 +1,50 @@
+import { registerSchema } from "@/server/schema/auth/auth.schema";
+import { findUserByEmail } from "@/server/services/user.service";
+import { NextResponse } from "next/server";
+import prisma from "@/server/db/prisma";
+import argon from "argon2";
+import { ZodError } from "zod";
+
+export async function POST(request: Request) {
+  try {
+    const body = registerSchema.parse(await request.json());
+
+    const user = await findUserByEmail(body.email);
+
+    if (!user) {
+      return new NextResponse("User Already Exists", {
+        status: 409,
+      });
+    }
+
+    const hashed = await argon.hash(body.password);
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: body.email,
+        username: body.username,
+        password: hashed,
+      },
+    });
+
+    if (!newUser) {
+      return new NextResponse("Failed To Create The Resource", {
+        status: 500,
+      });
+    }
+
+    return new NextResponse("User Created", {
+      status: 200,
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return new NextResponse("Invalid Credentials", {
+        status: 400,
+      });
+    }
+
+    return new NextResponse("Internal Server Error, Try again", {
+      status: 500,
+    });
+  }
+}
