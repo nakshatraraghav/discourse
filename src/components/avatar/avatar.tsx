98c89fdfebc45fb024085accc8eea4ed0dee71ab
nakshatra raghav <combatrickshaw@gmail.com>
2023-10-30 12:33:50 +0530

    (fix): pass styles to the avatar component

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -3,14 +3,23 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import { ClassValue } from "clsx";
+import { classNames } from "uploadthing/client";
 
 interface AvatarProps {
   image?: string;
+  className?: ClassValue;
 }
 
-export function Avatar({ image }: AvatarProps) {
+export function Avatar({ image, className }: AvatarProps) {
   return (
-    <UiAvatar className="w-[48px] h-[48px] mb-6 hover:opacity-90 hover:rounded-2xl transition-all duration-150">
+    <UiAvatar
+      className={cn(
+        "w-[48px] h-[48px] mb-6 hover:opacity-90 hover:rounded-2xl transition-all duration-150",
+        className
+      )}
+    >
       <AvatarImage src={image} alt="avatar" />
       <AvatarFallback className="text-sm">User</AvatarFallback>
     </UiAvatar>
