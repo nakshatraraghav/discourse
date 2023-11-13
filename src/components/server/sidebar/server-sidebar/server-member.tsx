cdca18d183b4e932fd5158ce2f0714b8f5fefdda
nakshatra <combatrickshaw@gmail.com>
2023-11-13 18:37:34 +0530

    (fix): cannot create conversation with yourself

--- src/components/server/sidebar/server-sidebar/server-member.tsx
+++ src/components/server/sidebar/server-sidebar/server-member.tsx
@@ -11,6 +11,9 @@ import { cn } from "@/lib/utils";
 import { Crown } from "lucide-react";
 import { LockClosedIcon, LockOpen2Icon } from "@radix-ui/react-icons";
 
+import { useSession } from "next-auth/react";
+import { useToast } from "@/hooks/use-toast";
+
 interface ServerMemberProps {
   member: Member & {
     user: User;
@@ -28,10 +31,25 @@ const iconMap = {
 export function ServerMember({ member, server, role }: ServerMemberProps) {
   const params = useParams();
   const router = useRouter();
+  const { data: session } = useSession();
+  const { toast } = useToast();
 
   const Icon = iconMap[member.role];
 
+  if (!session && session !== null) {
+    router.push("/login");
+  }
+
   function onClick() {
+    if (session?.user.id === member.user.id) {
+      toast({
+        variant: "destructive",
+        title: "You can't DM yourself!",
+        description: "Cannot open a Conversation with yourself.",
+      });
+
+      return;
+    }
     router.push(`/server/${params?.id}/conversation/${member.id}`);
   }
 
@@ -40,7 +58,10 @@ export function ServerMember({ member, server, role }: ServerMemberProps) {
       onClick={onClick}
       variant={"ghost"}
       size={"sm"}
-      className="group rounded-md gap-x-2 w-full mb-1 truncate"
+      className={cn(
+        "group rounded-md gap-x-2 w-full mb-1",
+        params.memberId === member.id && "text-white bg-secondary/90"
+      )}
     >
       <div className="w-full flex items-center space-x-1">
         <Icon
@@ -48,7 +69,7 @@ export function ServerMember({ member, server, role }: ServerMemberProps) {
             "text-zinc-500 group-hover:text-zinc-300",
             params.memberId === member.id && "text-white"
           )}
-          size={15}
+          size="15"
         />
         <p
           className={cn(
