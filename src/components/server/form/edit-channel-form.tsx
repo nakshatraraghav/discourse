55fe51ea26012fd2f4072b5f481014efc2594e53
nakshatra <combatrickshaw@gmail.com>
2023-11-13 12:45:18 +0530

    (fix): band-aid-solution to edit form schema validation error

--- src/components/server/form/edit-channel-form.tsx
+++ src/components/server/form/edit-channel-form.tsx
@@ -54,6 +54,13 @@ export function EditChannellForm({
 
   async function onSubmit(data: editChannelFormType) {
     try {
+      if (data.name === "" || data.name.toLocaleLowerCase() === "general") {
+        form.setError("name", {
+          message: "Invalid Name, Name cannot be empty or General",
+        });
+        throw new Error("invalid_error");
+      }
+
       await axios.patch(`/api/channels/${channelId}`, {
         name: data.name,
         serverId,
@@ -66,7 +73,17 @@ export function EditChannellForm({
         description: `Name updated to ${data.name}`,
       });
       router.refresh();
-    } catch (error) {
+    } catch (error: any) {
+      if (error.message === "invalid_error") {
+        toast({
+          variant: "destructive",
+          title: "Edit Channel Failed",
+          description:
+            "Failed You cannot set Channel Name to empty string or General",
+        });
+        return;
+      }
+
       toast({
         variant: "destructive",
         title: "Edit Channel Failed",
