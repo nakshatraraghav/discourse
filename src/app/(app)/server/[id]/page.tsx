485b54ab71d41d3a8bd9e87b997c0fb7687a7fc9
nakshatra <combatrickshaw@gmail.com>
2023-11-13 14:57:37 +0530

    (feat): redirect to the first general channel

--- src/app/(app)/server/[id]/page.tsx
+++ src/app/(app)/server/[id]/page.tsx
@@ -1,13 +1,52 @@
-export default function ServerPage({
-  params,
-}: {
+import prisma from "@/server/db/prisma";
+import { getServerSession } from "next-auth";
+import { options } from "@/server/auth";
+import { redirect } from "next/navigation";
+
+interface ServerPageProps {
   params: {
     id: string;
   };
-}) {
-  return (
-    <div className="bg-[#ffffff] dark:bg-[#36363837] w-full h-screen">
-      server id is: {params.id}
-    </div>
-  );
+}
+
+export default async function ServerPage({ params }: ServerPageProps) {
+  
+  const session = await getServerSession(options);
+
+  if (!session) {
+    return redirect("/login")
+  }
+  
+  const server = await prisma.server.findMany({
+    where: {
+      id: params.id,
+      members: {
+        some: {
+          userId: session.user.id
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: {
+            equals: "general"
+          }
+        },
+        take: 1
+      }
+    }
+  })
+
+  const channel = server[0].channels;
+
+  if (!channel) {
+    return <div>General Channel Was Somehow deleted</div>
+  }
+
+  const general = channel[0];
+
+  console.log(general)
+
+  return redirect(`/server/${params.id}/channel/${general.id}}`)
 }
