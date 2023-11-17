d0212a8f2324290e5c063a18224d9cf21f23d991
nakshatra <combatrickshaw@gmail.com>
2023-11-17 15:02:57 +0530

    react dev tools

---
+++ src/context/query-provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export function QueryProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
