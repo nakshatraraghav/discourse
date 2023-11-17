effdfb57141604c2f77badbafc77651b8852bb75
nakshatra <combatrickshaw@gmail.com>
2023-11-17 15:25:32 +0530

    (add): types for messages

--- types.ts
+++ types.ts
@@ -1,6 +1,8 @@
-import { Server, Member, Channel, User } from "@prisma/client";
+import { Server, Member, Channel, User, Message } from "@prisma/client";
 
 export type ServerWithMembersWithUser = Server & {
   members: (Member & { user: User })[];
   channels: Channel[];
 };
+
+export type MessageWithUser = (Message & { member: Member & { user: User } })[];
