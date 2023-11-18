51ec25954fd699648219c5e669a49be3cf8f15d3
nakshatra <combatrickshaw@gmail.com>
2023-11-18 19:36:56 +0530

    (nextjs): bugfix with livekit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+    return config;
+  },
   images: {
-    domains: [
-      "utfs.io"
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "utfs.io"
+      }
     ]
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
