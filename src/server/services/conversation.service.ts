78a54870dd7dabb8bfbbb33b03e99a521057b8ef
nakshatra <combatrickshaw@gmail.com>
2023-11-18 17:00:19 +0530

    (fix): 2 conversations being created

--- src/server/services/conversation.service.ts
+++ src/server/services/conversation.service.ts
@@ -1,76 +1,72 @@
 import prisma from "@/server/db/prisma";
 
-interface ConversationInterface {
-  memberOneId: string;
-  memberTwoId: string;
-}
-
-export async function findConversation({ memberOneId, memberTwoId }: ConversationInterface) {
-  try {
-    const conversation = await prisma.conversations.findFirst({
-      where: {
-        memberOneId,
-        memberTwoId
-      },
-      include: {
-        memberOne: {
-          include: {
-            user: true
-          }
-        },
-        memberTwo: {
-          include: {
-            user: true
-          }
-        }
-      }
-    })
-
-    return conversation;
-  } catch(error) {
-    console.log("Error finding conversation", error)
-    return null;
-  }
-}
-
-export async function createConversation({ memberOneId, memberTwoId } : ConversationInterface) {
-  try {
-    const conversation = await prisma.conversations.create({
-      data: {
-        memberOneId,
-        memberTwoId
-      },
-      include: {
-        memberOne: {
-          include: {
-            user: true
-          }
-        },
-        memberTwo: {
-          include: {
-            user: true
-          }
-        }
-      }
-    })
-
-    return conversation
-  } catch(error) {
-    console.log("Error creating conversation", error)
-    return null;
-  }
-}
-
-export async function findOrCreateConversation({memberOneId, memberTwoId }: ConversationInterface) {
-
-  const conversation = await findConversation({memberOneId, memberTwoId}) || await findConversation({memberOneId, memberTwoId});
+export async function getOrCreateConversation(
+  memberOneId: string,
+  memberTwoId: string
+) {
+  let conversation =
+    (await findConversation(memberOneId, memberTwoId)) ||
+    (await findConversation(memberTwoId, memberOneId));
 
   if (!conversation) {
-    return await createConversation({memberOneId, memberTwoId});
-  } else {
-    return conversation;
+    conversation = await createConversation(memberOneId, memberTwoId);
   }
-  
 
+  return conversation;
 }
 
+export async function findConversation(
+  memberOneId: string,
+  memberTwoId: string
+) {
+  try {
+    return await prisma.conversations.findFirst({
+      where: {
+        AND: [{ memberOneId: memberOneId }, { memberTwoId: memberTwoId }],
+      },
+      include: {
+        memberOne: {
+          include: {
+            user: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    console.log("find conversation error", error);
+    return null;
+  }
+}
+
+export async function createConversation(
+  memberOneId: string,
+  memberTwoId: string
+) {
+  try {
+    return await prisma.conversations.create({
+      data: {
+        memberOneId: memberOneId,
+        memberTwoId: memberTwoId,
+      },
+      include: {
+        memberOne: {
+          include: {
+            user: true,
+          },
+        },
+        memberTwo: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    return null;
+  }
+}
