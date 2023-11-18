e541d16b23b516092ed96977f2f42377cd9e1e90
nakshatra <combatrickshaw@gmail.com>
2023-11-18 17:02:53 +0530

    (badge): add more meaningfull badge message

--- src/components/chat/chat-header-socket-status.tsx
+++ src/components/chat/chat-header-socket-status.tsx
@@ -12,17 +12,17 @@ export function ChatHeaderSocketStatus() {
       {status === "initial" && <Badge>Socket: Initializing...</Badge>}
       {status === "connected" && (
         <Badge className="bg-green-700 hover:bg-green-800 text-xs font-semibold text-zinc-300">
-          Socket: Connected
+          Real Time Updates
         </Badge>
       )}
       {status === "disconnected" && (
         <Badge className="bg-red-600 hover:bg-red-700 text-xs font-semibold text-zinc-300">
-          Socket: Disconnected
+          Disconnected
         </Badge>
       )}
       {status === "failure" && (
         <Badge className="bg-yellow-600 hover:bg-red-700 text-xs font-semibold text-white">
-          Socket: Switching To Long Polling
+          Switching To Long Polling
         </Badge>
       )}
     </div>
