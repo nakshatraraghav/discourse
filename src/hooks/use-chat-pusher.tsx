2a9b423646ef19288379dc06d5557a7eba3d4602
nakshatra <combatrickshaw@gmail.com>
2023-11-18 13:12:29 +0530

    (pusher): implement chat socket

---
+++ src/hooks/use-chat-pusher.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useEffect } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useSocketStore } from "@/store/socket-status";
+
+import { pusherClient, events } from "@/lib/pusher";
+
+import { MessageWithUser } from "../../types";
+
+interface useChatPusherProps {
+  chatId: string;
+  queryKey: string;
+}
+
+export function useChatPusher({ chatId, queryKey }: useChatPusherProps) {
+  const { setStatus } = useSocketStore();
+
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const channel = pusherClient.subscribe(chatId);
+
+    channel.bind("pusher:subscription_succeeded", () => {
+      setStatus("connected");
+    });
+
+    channel.bind("pusher:subscription_error", (error: any) => {
+      console.log("pusher subscription error", error);
+      setStatus("failure");
+    });
+
+    channel.bind(events.addMessage, (message: MessageWithUser) => {
+      queryClient.setQueryData([queryKey], (staleData: any) => {
+        if (!staleData || !staleData.pages || staleData.pages.length === 0) {
+          return {
+            pages: [
+              {
+                items: [message],
+              },
+            ],
+          };
+        }
+
+        const newPage = [...staleData.pages];
+
+        newPage[0] = {
+          ...newPage[0],
+          items: [message, ...newPage[0].items],
+        };
+
+        return {
+          ...staleData,
+          pages: newPage,
+        };
+      });
+    });
+
+    channel.bind(events.updateMessage, (message: MessageWithUser) => {
+      queryClient.setQueryData([queryKey], (staleData: any) => {
+        if (!staleData || !staleData.pages || staleData.pages.length === 0) {
+          return staleData;
+        }
+
+        const updatedData = staleData.pages.map((page: any) => ({
+          ...page,
+          items: page.items.map((mesg: MessageWithUser) => {
+            if (mesg.id === message.id) {
+              return message;
+            }
+            return mesg;
+          }),
+        }));
+
+        return {
+          ...staleData,
+          pages: updatedData,
+        };
+      });
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [chatId, queryClient, queryKey, setStatus]);
+}
